2f59b62769abbed7da03a16c8c66218868be7632
Chris Maddox <tyre77@gmail.com>
2012-07-02 18:21:16 -0400

    sounds reasonable

--- config/initializers/resque_schedule.rb
+++ config/initializers/resque_schedule.rb
@@ -2,9 +2,12 @@ require 'resque_scheduler'
 require 'resque/scheduler'
 
 if Rails.env.production?
+  uri = URI.parse(ENV['REDISTOGO_URL'])
   Resque.redis = ENV['REDISTOGO_URL']
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   Resque.redis = 'localhost:6379'
+  $redis = Redis.new
 end
 
 Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
